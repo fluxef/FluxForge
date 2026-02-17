6aae9f9d89faf933efdbbf31817d78c116bef831
Andreas F. <af@ins.de>
2026-02-17 14:32:03 +0100

    file "/examples/mysql2postgres.toml" will now be baked into binary as default if no "--config" option is set.

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,8 @@
 use crate::core::ForgeConfig;
 use std::error::Error;
 
-// this files will be baked into binary as default if no --config option is used
-const DEFAULT_CONFIG_STR: &str = include_str!("default_mapping.toml");
+// this file will be baked into binary as default if no --config option is used
+const DEFAULT_CONFIG_STR: &str = include_str!("../examples/mysql2postgres.toml");
 
 /// load config file
 pub fn load_config(user_path: Option<std::path::PathBuf>) -> Result<ForgeConfig, Box<dyn Error>> {
@@ -18,6 +18,7 @@ pub fn load_config(user_path: Option<std::path::PathBuf>) -> Result<ForgeConfig,
 pub fn get_config_file_path(user_path: Option<std::path::PathBuf>) -> String {
     match user_path {
         Some(path) => path.to_string_lossy().to_string(),
-        None => "default_mapping.toml".to_string(),
+        //        None => "default_mapping.toml".to_string(),
+        None => "../examples/mysql2postgres.toml".to_string(),
     }
 }
